b8408dfa2ac79e8ab20a91d5f72dd538864a0a27
Graphene101102 <vietvcg.tm2002@gmail.com>
2024-09-06 18:02:09 +0700

    2- UI design HomePage (1st).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { Route, Routes } from 'react-router-dom'
 import Loginpage from './pages/auth/login/LoginPage'
 import HomePage from './pages/home/HomePage'
 import SignUpPage from './pages/auth/signup/SignUpPage'
+import Sidebar from './components/common/Sidebar'
+import RightPanel from './components/common/RightPanel'
 
 
 function App() {
     return (
         <div className='flex max-w-6xl mx-auto'>
+            <Sidebar />
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/signup' element={<SignUpPage />} />
             <Route path='/login' element={<Loginpage />} />
         </Routes>
+            <RightPanel />
     </div>
 
     )

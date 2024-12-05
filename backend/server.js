a98634712e92e724ce1034317a802c1df6c7c1eb
Graphene101102 <vietvcg.tm2002@gmail.com>
2024-12-05 15:01:56 +0700

    7 - prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import path from 'path';
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -21,6 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve()
 
 app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({extended: true})); //to parse form data
@@ -31,6 +33,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notification", notificationRoutes);
+ 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
